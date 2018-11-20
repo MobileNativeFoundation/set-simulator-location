41d54df83f9931a607ff1cd5fa8d30d10c60d411
Keith Smiley <keithbsmiley@gmail.com>
2018-11-20 10:43:58 -0800

    Guard it

--- sources/cli.swift
+++ sources/cli.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 func consumeArgument(flag: String, from arguments: inout ArraySlice<String>) -> String? {
-    if let argumentIndex = arguments.index(of: flag) {
-        let value = arguments.suffix(from: argumentIndex).dropFirst().joined(separator: " ")
-        if value.isEmpty {
-            exitWithUsage()
-        }
-
-        arguments = arguments.prefix(upTo: argumentIndex)
-        return value
+    guard let argumentIndex = arguments.index(of: flag) else {
+        return nil
     }
 
-    return nil
+    let value = arguments.suffix(from: argumentIndex).dropFirst().joined(separator: " ")
+    if value.isEmpty {
+        exitWithUsage()
+    }
+
+    arguments = arguments.prefix(upTo: argumentIndex)
+    return value
 }
 
 func createUUID(from string: String) throws -> UUID {
