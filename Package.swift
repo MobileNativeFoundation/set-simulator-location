9312defca54f5bed3d4d5cd581dd6f5b1395b2d3
Shoma Saito <shoma@tantakan.net>
2020-06-23 04:27:56 +0900

    Add executable product (#26)

--- Package.swift
+++ Package.swift
@@ -3,6 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "set-simulator-location",
+    products: [
+        .executable(
+            name: "set-simulator-location",
+            targets: [
+                "set-simulator-location"
+            ]
+        )
+    ],
     targets: [
         .target(
             name: "set-simulator-location",
